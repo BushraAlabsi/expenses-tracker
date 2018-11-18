d2e417e406e98d86b7f26cc8ba346faa24bf5745
BushraAlabsi <boushra-93@live.com>
2018-11-18 03:02:44 +0200

    creates the suntotal and add functions in backend

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -2,6 +2,10 @@ var db = require('../db');
 
 module.exports = {
     subtotal: (req, res) => {
+     db.expenses.sum('amount', { where: { category: req.params.category , month:req.params.month } })
+     .then(sum => {
+      res.json(sum);
+})
     },
     total: function (req, res) {
       db.expenses.sum('amount').then(sum => {
@@ -9,7 +13,13 @@ module.exports = {
         });
     },
     add: function (req, res) {
-    
+      db.expenses.create({ 
+        amount:req.body.amount,
+        category: req.body.category,
+        month: req.body.month}).
+      then(resutl => {
+        res.sendStatus(201);
+      });
     }
   
 };
